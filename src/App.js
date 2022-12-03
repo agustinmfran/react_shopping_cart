1d0e955d79586f27c16f4965d62e5af5eb4f36a0
agustinmfran <agustinmfran@gmail.com>
2022-12-03 12:09:12 +0100

    agregado navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
-import Productos from './components/Productos'
-
+import Productos from './components/Productos';
+import Layout from './components/Layout';
+import Title from './components/Title';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   state = {
@@ -13,10 +15,14 @@ class App extends Component {
   render(){
     return(
       <div>
-        <Productos 
-          agregarAlCarro={()=> console.log('no hace nada')}
-          productos={this.state.productos}
-        />
+        <Navbar />
+        <Layout>
+          <Title />
+          <Productos 
+            agregarAlCarro={()=> console.log('no hace nada')}
+            productos={this.state.productos}
+          />
+        </Layout>        
       </div>
     )
   };
